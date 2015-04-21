282302e1b8f0f8fcb85dbe1a179727c5df983cde
idlove <meowmaster@yandex.by>
2015-04-21 17:01:42 +0300

    Encryption functionality fix

---
+++ data/encryption.js
@@ -0,0 +1,45 @@
+/* Some shitty globals~ */
+const RSA_BITS = 4096;
+
+
+function createPrimaryKey(){
+    if(typeof localStorage!=typeof undefined){
+        if(localStorage.getItem('masterpass')){
+            var masterKeyPass = localStorage.getItem('masterpass');
+            var masterKey = cryptico.generateRSAKey(masterKeyPass, RSA_BITS);
+            return masterKey;
+        }else{
+            var masterKeyPass = prompt('Enter your new master key pass! \n (Will be used for key generation)');
+            localStorage.setItem('masterpass',masterKeyPass);
+            return createPrimaryKey();
+        }
+    }
+
+    return false;
+}
+
+function getOwnPublicKey(masterKey){
+    if(typeof masterKey!=typeof undefined){
+        return cryptico.publicKeyString(masterKey);
+    }
+
+    return false;
+}
+
+function encryptMessage(opponentsPublicKey,plainText){
+    if(typeof opponentsPublicKey!=typeof undefined){
+        var encryptedMessage = cryptico.encrypt(plainText, opponentsPublicKey);
+        return encryptedMessage;
+    }
+
+    return false;
+}
+
+function decryptMessage(encryptedMessage,masterKey){
+    if(typeof encryptedMessage!=typeof undefined && typeof masterKey!=typeof undefined){
+        var decryptedMessage = cryptico.decrypt(encryptedMessage, masterKey);
+        return decryptedMessage;
+    }
+
+    return false;
+}
